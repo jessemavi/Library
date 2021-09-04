623a425abb53bd52c4ea8890d70a49b18a6f4c56
Jesse Mavi <jessemavi@Jesses-Air.attlocal.net>
2021-09-03 23:58:55 -0500

    Adds react client app.

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <p>Hello, world!</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
