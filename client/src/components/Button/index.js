c6960b28ba31aa63802c540c99ac132da029d2f1
Jesse Mavi <jessemavi@Jesses-Air.attlocal.net>
2021-09-06 00:06:03 -0500

    Adds style for login page

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+// reusable Button component
 function Button({ className, disabled, onClick, text, type }) {
   let buttonClasses = "bg-red-500 hover:bg-red-700 font-semibold rounded px-4 py-2 shadow hover:shadow-md focus:outline-none focus:shadow-outline text-white text-sm sm:text-base";
 
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,5 +1,80 @@
+import { useState } from "react";
+
+import Button from "../../components/Button";
+import TextInput from "../../components/TextInput";
+
 function Login() {
-  return <p>This is the page where users authenticate.</p>
+  const [isExistingUser, setIsExistingUser] = useState(true);
+
+  return (
+    <div className="bg-gray-50 flex items-center justify-center
+    min-h-screen">
+      <div className="bg-white shadow p-8 max-w-sm w-10/12">
+        <h1 className="mb-4 text-2xl">Welcome to Library</h1>
+        <form>
+          <TextInput
+            className="mb-4"
+            hiddenLabel
+            id="username"
+            inputWidthClass="w-full"
+            label="Username"
+            placeholder="Your username"
+            type="text"
+          />
+          {isExistingUser && (
+            <>
+              <TextInput
+                className="mb-4"
+                hiddenLabel
+                id="email"
+                inputWidthClass="w-full"
+                label="Email"
+                placeholder="Your email"
+                type="email"
+              />
+              <TextInput
+                className="mb-4"
+                hiddenLabel
+                id="name"
+                inputWidthClass="w-full"
+                label="Full Name"
+                placeholder="Your full name"
+                type="text"
+              />
+            </>
+          )}
+          <TextInput
+            className="mb-8"
+            hiddenLabel
+            id="password"
+            inputWidthClass="w-full"
+            label="Password"
+            placeholder="Your password"
+            type="password"
+          />
+          <div className="flex items-center">
+            <Button
+              className="mr-2"
+              text={isExistingUser ? "Log in" : "Sign Up"}
+              type="submit"
+            />
+            <p className="text-gray-400">
+              {isExistingUser ? "New here?" : "Already joined?"}
+            </p>
+            <button
+              className="text-red-500 hover:text-red-700 ml-1 focus:outline-none hover:underline"
+              onClick={event => {
+                event.preventDefault();
+                setIsExistingUser(state => !state);
+              }}
+            >
+              {isExistingUser ? "Sign up." : "Log in."}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
