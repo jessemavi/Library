a8ef8472f3b5c723c757f431d16f1fcbdbe53198
Jesse Mavi <jessemavi@Jesses-Air.attlocal.net>
2021-09-07 12:28:21 -0500

    Adds login and signup mutations in client

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,17 +1,40 @@
 import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { useHistory } from "react-router-dom";
 
+import { Login as LoginMutation, SignUp } from "../../graphql/mutations";
 import Button from "../../components/Button";
 import TextInput from "../../components/TextInput";
 
 function Login() {
   const [isExistingUser, setIsExistingUser] = useState(true);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const history = useHistory();
+
+  const onCompleted = () => history.push('/home');
+
+  const [login, { error, loading }] = useMutation(LoginMutation, { onCompleted });
+  const [signup] = useMutation(SignUp, { onCompleted });
 
   return (
-    <div className="bg-gray-50 flex items-center justify-center
-    min-h-screen">
+    <div className="bg-gray-50 flex items-center justify-center min-h-screen">
       <div className="bg-white shadow p-8 max-w-sm w-10/12">
         <h1 className="mb-4 text-2xl">Welcome to Library</h1>
-        <form>
+        <form
+          onSubmit={async event => {
+            event.preventDefault();
+            if (isExistingUser) {
+              await login({ variables: { username, password } }).catch(err => console.error(err))
+            } else {
+              await signup({ 
+                variables: { input: { email, name, password, username } }
+              }).catch(err => console.error(err))
+            }
+          }}
+        >
           <TextInput
             className="mb-4"
             hiddenLabel
@@ -20,8 +43,10 @@ function Login() {
             label="Username"
             placeholder="Your username"
             type="text"
+            onChange={event => setUsername(event.target.value)}
+            value={username}
           />
-          {isExistingUser && (
+          {!isExistingUser && (
             <>
               <TextInput
                 className="mb-4"
@@ -31,6 +56,8 @@ function Login() {
                 label="Email"
                 placeholder="Your email"
                 type="email"
+                onChange={event => setEmail(event.target.value)}
+                value={email}
               />
               <TextInput
                 className="mb-4"
@@ -40,6 +67,8 @@ function Login() {
                 label="Full Name"
                 placeholder="Your full name"
                 type="text"
+                onChange={event => setName(event.target.value)}
+                value={name}
               />
             </>
           )}
@@ -51,12 +80,15 @@ function Login() {
             label="Password"
             placeholder="Your password"
             type="password"
+            onChange={event => setPassword(event.target.value)}
+            value={password}
           />
           <div className="flex items-center">
             <Button
               className="mr-2"
               text={isExistingUser ? "Log in" : "Sign Up"}
               type="submit"
+              disabled={loading}
             />
             <p className="text-gray-400">
               {isExistingUser ? "New here?" : "Already joined?"}
@@ -71,6 +103,9 @@ function Login() {
               {isExistingUser ? "Sign up." : "Log in."}
             </button>
           </div>
+          {error && (
+            <p className="mt-4 text-red-500 text-sm">{error.message}</p>
+          )}
         </form>
       </div>
     </div>
